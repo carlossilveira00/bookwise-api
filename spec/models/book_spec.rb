7875b0a647cc13801897edb1ab6e09563e226673
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-01-14 21:43:05 -0100

    Create book model tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,6 +3,15 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   describe "Validates presence" do
       it { should validate_presence_of(:title) }
+      it { should validate_presence_of(:description) }
+      it { should validate_presence_of(:author) }
+      it { should validate_presence_of(:category) }
+      it { should validate_presence_of(:ISBN) }
   end
+
+  describe "Validates Uniqueness" do
+    it { should validate_uniqueness_of(:ISBN) }
+  end
+
 
 end
