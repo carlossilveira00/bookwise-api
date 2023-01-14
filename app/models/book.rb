e8aba8540de970d39661c0c3a9598ccd0e71d02d
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-01-14 21:50:39 -0100

    Add validations in Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+  validates :tittle, presence: true
+  validates :description, presence: true
+  validates :author, presence: true
+  validates :category, presence: true
+  validates :ISBN, presence: true
+  validates :ISBN, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_14_222748) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_14_224459) do
   create_table "books", force: :cascade do |t|
-    t.string "title"
+    t.string "tittle"
     t.text "description"
     t.integer "pages"
     t.string "author"
