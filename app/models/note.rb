e90900fb34bbc920705638b54884be3021573803
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-01-17 09:24:06 -0100

    Add the test to validate user_id presence and add the validation in the model.

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,5 +2,6 @@ class Note < ApplicationRecord
   belongs_to :user_library
   validates :title, presence: true
   validates :user_library_id, presence: true
+  validates :user_id, presence: true
   validates :body, presence: true
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Note, type: :model do
     it { should validate_presence_of(:user_library_id) }
     it { should validate_presence_of(:body) }
     it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:user_id) }
   end
 
 end
