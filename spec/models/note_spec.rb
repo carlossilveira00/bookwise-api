569196bf88e8e5ae57587b56ada4c27568d530b4
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-01-20 17:07:20 -0100

    Delete validation of title and body from notes, because those fields can be empty when you create a notes.

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.describe Note, type: :model do
   describe 'Validates presence' do
     it { should validate_presence_of(:user_library_id) }
-    it { should validate_presence_of(:body) }
-    it { should validate_presence_of(:title) }
     it { should validate_presence_of(:user_id) }
   end
 
