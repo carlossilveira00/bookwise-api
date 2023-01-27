9751050f62d3dfcbd55e2e5b0f904c6d53cd154d
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-01-27 16:49:10 -0100

    Create auth_user method in the member controller to authenticate the user through the Authorization token

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,8 +1,15 @@
 class MembersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :auth_user
 
   def index
     render json: current_user
+  end
+
+  private
+
+  def auth_user
+    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.application.credentials.fetch(:secret_key_base)).first
+    User.find(jwt_payload['sub'])
   end
 
 end
