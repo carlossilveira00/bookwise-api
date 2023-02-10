8b6ee9b602c0d6f649514df2e4b3a02ff4874299
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-02-10 18:00:57 -0100

    Fetch all the information for creating a book.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ user_serialized = URI.open(url).read
 user = JSON.parse(user_serialized)
 
 user["items"].each do |book|
-  # p book['volumeInfo']
+  p book['volumeInfo']
   # p "********"
   # p book['volumeInfo']['title']
   # p "********"
@@ -16,7 +16,17 @@ user["items"].each do |book|
   # p "********"
   # p book['volumeInfo']['authors'][0]
   # p "********"
-  p book['volumeInfo']['industryIdentifiers']
+  # p book['volumeInfo']['industryIdentifiers'][0]['identifier'] unless book['volumeInfo']['industryIdentifiers'].nil?
+  # p "********"
+  # p book['volumeInfo']['pageCount']
+  # p "********"
+  # p book['volumeInfo']['categories'][0]
+  # p "********"
+  # p book['volumeInfo']['imageLinks']['thumbnail']
+  # p "********"
+  # p book['volumeInfo']['averageRating']
+  # p "********"
+  # p book['volumeInfo']['publishedDate']
   p "----------------------"
 end
 # https://www.googleapis.com/books/v1/volumes?q=subject:self-help
