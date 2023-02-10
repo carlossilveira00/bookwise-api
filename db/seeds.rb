9a3d648544f9776bfb48312b90bf80c053e247fc
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-02-10 18:31:00 -0100

    Create seeds file and loop through categories from Google book API to create books.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,29 @@
-require "json"
-require "open-uri"
+require "json";
+require "open-uri";
 
-url = "https://www.googleapis.com/books/v1/volumes?q=subject:science&maxResults=40"
-user_serialized = URI.open(url).read
-user = JSON.parse(user_serialized)
+categories = ['Adult', 'Anthologies', 'Art', 'Biographies', 'Body', 'Business', 'Children', 'Comics', 'Cooking', ' Contemporary', 'Crime', 'Engineering', 'Entertainment', 'Fantasy', 'Fiction', 'Food', 'General', 'Health', 'History', 'Horror', 'Investing', 'Literature', 'Media-help', 'Memoirs', 'Mystery', 'Nonfiction', 'Religion', 'Romance', 'Science', 'Sports', 'Superheroes', 'Technology', 'Thrillers', 'Travel', 'Young']
 
-user["items"].each do |book|
-  p book['volumeInfo']
-  # p "********"
-  # p book['volumeInfo']['title']
-  # p "********"
-  # p book['volumeInfo']['description']
-  # p "********"
-  # p book['volumeInfo']['publisher']
-  # p "********"
-  # p book['volumeInfo']['authors'][0]
-  # p "********"
-  # p book['volumeInfo']['industryIdentifiers'][0]['identifier'] unless book['volumeInfo']['industryIdentifiers'].nil?
-  # p "********"
-  # p book['volumeInfo']['pageCount']
-  # p "********"
-  # p book['volumeInfo']['categories'][0]
-  # p "********"
-  # p book['volumeInfo']['imageLinks']['thumbnail']
-  # p "********"
-  # p book['volumeInfo']['averageRating']
-  # p "********"
-  # p book['volumeInfo']['publishedDate']
-  p "----------------------"
+
+categories.each do |category|
+  url = "https://www.googleapis.com/books/v1/volumes?q=subject:#{category}&maxResults=40"
+  books_serialized = URI.open(url).read
+  books = JSON.parse(books_serialized)
+
+  books['items'].each do |book|
+    unless book['volumeInfo']['industryIdentifiers'].nil?
+      Book.create!({
+        title: book['volumeInfo']['title'],
+        description: book['volumeInfo']['description'],
+        pages: book['volumeInfo']['pageCount'],
+        author: book['volumeInfo']['authors'][0],
+        category: book['volumeInfo']['categories'][0],
+        thumbnail_url: book['volumeInfo']['imageLinks']['thumbnail'],
+        publisher: book['volumeInfo']['publisher'],
+        ISBN: book['volumeInfo']['industryIdentifiers'][0]['identifier'],
+        published_date: book['volumeInfo']['publishedDate'],
+        rating: book['volumeInfo']['averageRating']
+      })
+    end
+  end
+  puts "Completed category: #{category}"
 end
-# https://www.googleapis.com/books/v1/volumes?q=subject:self-help
