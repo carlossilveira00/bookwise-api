e61daa9aaf766da1689339118857e98e09cf4d7b
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-02-17 21:26:15 -0100

    Create tests for the book_notes method in the notes controller.

--- spec/requests/notes_spec.rb
+++ spec/requests/notes_spec.rb
@@ -167,4 +167,38 @@ RSpec.describe NotesController, type: :controller do
     end
   end
 
+  describe 'GET /book-notes/' do
+    before(:each) do
+      # Create a user and a book
+      @user = User.create(email: 'test@example.com', password: 'password')
+      @book = Book.create(title: 'Test Book', description:'Hello World.', author: 'Test Author', category: 'Fiction', ISBN:'HELLOWORLD00')
+
+      # Create a user_book
+      @user_book = UserLibrary.create(user_id: @user.id, book_id: @book.id)
+
+      # Create a note
+      @note = Note.create(user_id: @user.id, user_library_id: @user_book.id, title: 'First Note', body: 'Hello World')
+    end
+
+    context 'Retrieves all the notes for a specific book from a specific user.' do
+      it 'returns a 200 status code.' do
+        get :book_notes, params: { user_book_id: @user_book.id }
+
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'returns the response in json' do
+        get :book_notes, params: { user_book_id: @user_book.id }
+
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+
+      it 'return the correct number of notes' do
+        get :book_notes, params: { user_book_id: @user_book.id }
+        json_response = JSON.parse(response.body)
+
+        expect(json_response['body'].lenth).to eq 1
+      end
+    end
+  end
 end
